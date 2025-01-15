c8cc5343df7e0beffc941c26f4cb3c2029f8152d
Leo Gimenez <leogimenez.dev@gmail.com>
2025-01-15 14:52:53 -0300

    se agregan librerías

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.button import Button
